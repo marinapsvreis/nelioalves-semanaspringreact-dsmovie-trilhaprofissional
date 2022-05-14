5551470faec01ab712309ac6c445bbed3f553866
Marina Portugal <marinapsvreis@gmail.com>
2022-05-14 15:59:11 -0300

    first request

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from 'axios'
 import MovieCard from 'components/movie-card'
 import Pagination from 'components/pagination'
+import { BASE_URL } from 'utils/requests'
 
 function Listing() {
+  //forma errada
+  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+  .then(response => {
+    console.log(response.data)
+  })
+
   return (
     <>
       <Pagination />
